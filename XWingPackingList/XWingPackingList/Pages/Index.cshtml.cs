91d80c1746afd4190ccc3cf0e7ea66b59bd24731
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-08-17 19:16:23 +0100

    Initia; workings

---
+++ XWingPackingList/XWingPackingList/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace XWingPackingList.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        [Required]
+        public string XWSText { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            if (!string.IsNullOrWhiteSpace(XWSText))
+            {
+                try
+                {
+                    object testJson = JObject.Parse(XWSText);
+                }
+                catch (JsonReaderException)
+                {
+                    ErrorMessage = "Bad XWS";
+                }
+            }
+        }
+    }
+}
